180e0f82595ccfd2669d9905cba365aeba44b7b3
Ilya Kurpiets <kkzemeow@gmail.com>
2024-06-09 17:15:30 +0300

    added carousel

--- src/widgets/trending/Trending.tsx
+++ src/widgets/trending/Trending.tsx
@@ -1,9 +1,28 @@
 import { Card } from "../../entities/card/Card";
 import { useGetTrendingQuery } from "../../shared/api/trending.api";
 import { TitleT } from "../../shared/types";
-
 import styles from "./Trending.module.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 export const Trending = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 4000, min: 1824 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1823, min: 1024 },
+      items: 3,
+    },
+    tablet2: {
+      breakpoint: { max: 1023, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   const { data = [], isLoading } = useGetTrendingQuery({
     refetchOnMount: false,
     refetchOnWindowFocus: false,
@@ -13,11 +32,16 @@ export const Trending = () => {
     <div className={styles.container}>
       <p className={styles.title}>Trending</p>
       {isLoading && <p>Loading...</p>}
-      <div className={styles.cards}>
+      <Carousel
+        responsive={responsive}
+        autoPlay
+        arrows={false}
+        className={styles.carousel}
+      >
         {data.map((title: TitleT, index: number) => (
           <Card key={index} title={title} />
         ))}
-      </div>
+      </Carousel>
       <button>See more </button>
     </div>
   );
