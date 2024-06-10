ac9fa1924223549bb2e8dd06afe8d87bf0e6c17b
Ilya Kurpiets <kkzemeow@gmail.com>
2024-06-11 00:23:51 +0300

    titlepage without player

--- src/pages/titlePage/TitlePage.tsx
+++ src/pages/titlePage/TitlePage.tsx
@@ -1,14 +1,43 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styles from "./TitlePage.module.scss";
 import { useGetTitleQuery } from "../../shared/api/trending.api";
 import { IMG_URL } from "../../shared/api/urlValues";
+import { StarSVG } from "../../shared/assets/StarSVG";
 export const TitlePage = () => {
   const { id } = useParams();
   const { data, isLoading } = useGetTitleQuery(id as string);
 
   return (
     <div className={styles.container}>
-      {data && <img src={`${IMG_URL}${data.posters.medium.url}`} alt="" />}
+      <Link to="/">
+        <p className={styles.back}>{"<"} Back</p>
+      </Link>
+      {data && (
+        <>
+          <p className={styles.title}>{data.names.ru}</p>
+          <p className={styles.titleEn}>{data.names.en}</p>
+          <div className={styles.content}>
+            <div className={styles.titleInfo}>
+              <div className={styles.info}>
+                <div>
+                  <div className={styles.moreInfo}>
+                    <StarSVG />
+                    <p>{data.in_favorites}</p>
+                    <p>{data.status.string}</p>
+                    <p>{data.season.year}</p>
+                  </div>
+                  <img
+                    src={`${IMG_URL}${data.posters.medium.url}`}
+                    className={styles.img}
+                    alt="title page"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.description}>{data.description}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
